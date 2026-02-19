6df568518de1cd9dc8c8564184a0fb100c16c608
Hess Wang <heisuewang@gmail.com>
2026-02-20 07:28:42 +0900

    Time: 3 ms (82.67%), Space: 20.7 MB (20.03%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,8 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        write = 0
+        k = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[write], nums[i] = nums[i], nums[write]
-                write += 1
+                nums[k], nums[i] = nums[i], nums[k]
+                k += 1
