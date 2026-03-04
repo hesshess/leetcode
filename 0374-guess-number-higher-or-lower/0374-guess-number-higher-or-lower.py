02e49a1673f0d45a2ece5ba2a20924a8468f61d2
Hess Wang <heisuewang@gmail.com>
2026-03-04 11:23:23 +0900

    Time: 47 ms (38.22%), Space: 19.1 MB (88.67%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -8,12 +8,12 @@
 class Solution:
     def guessNumber(self, n: int) -> int:
         low, high = 1, n
-        while low <= high:
+        while low < high:
             mid = (high + low) // 2
             if guess(mid) > 0:
                 low = mid + 1
-            elif guess(mid) < 0:
-                high = mid - 1
-            else: 
-                return mid
+            elif guess(mid) <= 0:
+                high = mid 
+        return low
+
         
