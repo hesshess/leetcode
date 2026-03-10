a4e5cba7dd43493abffd289784aa9a35a42b1adc
Hess Wang <heisuewang@gmail.com>
2026-03-10 15:34:10 +0900

    Time: 11 ms (22.14%), Space: 27.1 MB (7.62%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,6 +5,22 @@ class Solution:
         """
         n = len(nums)
         k %= n
-        nums[:] = nums[-k:] + nums[:-k] 
+        count = 0
+        start = 0
+
+        while count < n:
+            current = start
+            prev = nums[start]
+
+            while True:
+                next_idx = (current + k) % n
+                nums[next_idx], prev = prev, nums[next_idx]
+                current = next_idx
+                count += 1
+
+                if start == current:
+                    break
+            start += 1 
+            
 
             
