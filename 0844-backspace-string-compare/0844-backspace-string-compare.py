e55c962049500a74ca3f89103fa38ed3f884cfb1
Hess Wang <heisuewang@gmail.com>
2026-03-18 12:36:54 +0900

    Time: 3 ms (12.29%), Space: 19.2 MB (85.57%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,11 +1,12 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        i,j = len(s)-1, len(t)-1
-        skip_s = skip_t = 0
+        i, j = len(s) - 1, len(t) - 1
+        skip_s = 0
+        skip_t = 0
 
-        while i >=0 or j >= 0:
+        while i >= 0 or j >= 0:
             while i >= 0:
-                if s[i] == '#':
+                if s[i] == "#":
                     skip_s += 1
                     i -= 1
                 elif skip_s > 0:
@@ -13,24 +14,24 @@ class Solution:
                     i -= 1
                 else:
                     break
-            while j>= 0:
-                if t[j] == '#':
+
+            while j >= 0:
+                if t[j] == "#":
                     skip_t += 1
                     j -= 1
                 elif skip_t > 0:
                     skip_t -= 1
-                    j-=1
+                    j -= 1
                 else:
                     break
-            
-            if i >=0 and j >= 0:
+                    
+            if i >= 0 and j >= 0:
                 if s[i] != t[j]:
                     return False
             else:
-                if i >=0 or j >= 0:
+                if i >= 0 or j >= 0:
                     return False
             i -= 1
             j -= 1
 
         return True
-
